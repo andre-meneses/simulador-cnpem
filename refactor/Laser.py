7bd7835a45cd946132cd0c013d93deb9f8de74f4
Andre Meneses <andre_meneses2001@hotmail.com>
2024-01-10 17:17:10 -0300

    Transform module into class

--- refactor/Laser.py
+++ refactor/Laser.py
@@ -1,22 +1,37 @@
 import time
 from Mcp import Mcp
 
-# These functions control the laser, turning it on and off
-def switch_laser(mcp, switch='on'):
-    """
-    Turn on the laser by setting the GPIO pin to high (True).
-    :param mcp: Mcp2210 object for communication.
-    """
-    if switch=='on':
-        mcp.set_gpio_output_value(0, True)
-    else:
-        mcp.set_gpio_output_value(0, False)
+class LaserController:
+    def __init__(self, mcp):
+        self.mcp = mcp  # MCP2210 instance is passed during initialization
+
+    def switch_laser(self, switch='on'):
+        """
+        Turn on or off the laser by setting the GPIO pin.
+        :param switch: 'on' to turn on, 'off' to turn off.
+        """
+        if switch == 'on':
+            self.mcp.set_gpio_output_value(0, True)
+        else:
+            self.mcp.set_gpio_output_value(0, False)
+
+    def blink_laser(self, times):
+        """
+        Blink the laser on and off for a specified duration.
+        :param times: Duration to keep the laser on.
+        """
+        self.switch_laser('on')
+        time.sleep(times)
+        self.switch_laser('off')
+
 
 if __name__ == '__main__':
-    mcp = Mcp()  # Initialize the MCP2210
+    mcp = Mcp()  # Create an instance of Mcp
+    laser_controller = LaserController(mcp)  # Pass the Mcp instance to LaserController
+
     for i in range(1000):
-        switch_laser(mcp, switch='on')   # Turn on the laser
-        time.sleep(0.1) # Wait for 0.1 seconds
-        switch_laser(mcp, switch='off')   # Turn on the laser
-        time.sleep(0.1) # Wait for 0.1 seconds
+        laser_controller.switch_laser('on')   # Turn on the laser
+        time.sleep(0.1)                       # Wait for 0.1 seconds
+        laser_controller.switch_laser('off')  # Turn off the laser
+        time.sleep(0.1)                       # Wait for 0.1 seconds
 
