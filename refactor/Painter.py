6138040801e22a707d491051fa49fe2cbdf7f85c
Andre Meneses <andre_meneses2001@hotmail.com>
2024-01-11 14:33:47 -0300

    Add painter test

--- refactor/Painter.py
+++ refactor/Painter.py
@@ -1,4 +1,5 @@
 import socket
+import math
 import time
 from mcp2210 import Mcp2210, Mcp2210GpioDesignation, Mcp2210GpioDirection
 from Laser import LaserController
@@ -23,7 +24,6 @@ class Painter:
             pos_x_str = f"MWV:{pos_x}\r\n"
             arr_x = pos_x_str.encode('utf-8')
             self.socket_x.sendall(arr_x)
-            self.laser.switch_laser('on')
             time.sleep(self.tlaser)
             self.laser.switch_laser('off')
             pos_x += increment * self.calx
@@ -38,26 +38,51 @@ class Painter:
             pos_y_str = f"MWV:{pos_y}\r\n"
             arr_y = pos_y_str.encode('utf-8')
             self.socket_y.sendall(arr_y)
-            Laser.switch_laser(self.mcp, 'on')
             time.sleep(self.tlaser)
-            Laser.switch_laser(self.mcp, 'off')
+            self.laser.switch_laser('off')
             pos_y += increment * self.caly
             print(pos_y)
 
+    def paint_test(self):
+        posY =  -(60 / 2) * self.caly
+        npontos = 15 
+        for i in range(npontos):      
+            posYst = "MWV:"+str(posY)+"\r\n"
+            arrY = bytes(posYst, 'utf-8')  
+            self.socket_y.sendall(arrY)       
+            posX =  -(60 / 2) * self.calx
+            for j in range (npontos):        
+                posXst = "MWV:"+str(posX)+"\r\n"
+                arrX = bytes(posXst, 'utf-8')
+                self.socket_x.sendall(arrX)
+                self.laser.switch_laser('on')
+                time.sleep(self.tlaser)
+                self.laser.switch_laser('off')
+                posX = posX + 4 * calx
+                print("Posição x:", posX)
+            posY = posY + 4 * caly
+
+        
 if __name__ == '__main__':
     host_x = "192.168.0.11"  # Server's IP address
+    host_y = "192.168.1.10"  # Server's IP address
+
     port = 10001  # Port used by the server
+
     socket_x = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_y = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
     try:
         socket_x.connect((host_x, port))
+        socket_y.connect((host_y, port))
     except socket.error as e:
         print(f"Error connecting to socket: {e}")
-        # Handle error appropriately
 
     mcp = Mcp.Mcp()
     calx = 20 / (113.41 + 114.21)
     caly = 20 / (153.02 + 154.22)
     
-    painter = Painter(socket_x, None, calx, caly, mcp)  # Adjust as needed
-    painter.paint_x(60, 4)  # Example usage
+    painter = Painter(socket_x, socket_y, calx, caly, mcp)  # Adjust as needed
+
+    painter.paint_test()
 
