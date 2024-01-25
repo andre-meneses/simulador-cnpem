c0b6cbcb1b5a0406bd27c26e2b264e7628cf432a
Andre Meneses <andre_meneses2001@hotmail.com>
2024-01-25 15:50:00 -0300

    Latest commit

--- refactor/Contour.py
+++ refactor/Contour.py
@@ -10,6 +10,7 @@ class SilhouetteTo3D:
     def __init__(self):
         self.points = []
         self.points_cloud = []
+        self.coordinates = None
 
     def find_center(self, contour):
         M = cv2.moments(contour)
@@ -29,16 +30,10 @@ class SilhouetteTo3D:
         x = []
         y = []
         for point in contour:
-            # print(point)
-            # print(point[0])
             self.find_center(contour)
             x = point[0][0]
             y = point[0][1]
-            # plt.plot([point[0][0], point[0][1]], [self.cx, self.cy])
             self.points.append([self.radius(point[0]), np.deg2rad(theta), 186 - point[0][1]])
-
-        # plt.plot(x,y)
-        # plt.show()
 
     def cyl2cart(self, point):
         x = point[0] * np.cos(point[1]) 
@@ -64,25 +59,52 @@ class SilhouetteTo3D:
         ax.plot(x,y,z)
         plt.show()
 
-    def plot_shell(self):
+    def generate_solid(self):
         cloud = pv.PolyData(np.array(self.points_cloud))
-
-        # surf = cloud.delaunay_3d(alpha=20)
         surf = cloud.delaunay_3d()
-        # surf.save('mesh.obj')
-        surf.plot(show_edges=False)
         voxels = pv.voxelize(surf, check_surface=False)
-        # slices = voxels.slice_orthogonal()
-        # slices.plot(show_edges=True)
-        # coords = np.array(voxels.points)
-        # print(coords)
-        # voxels.save('tumor.obj')
-        # voxels.plot()
+        self.coordinates = np.array(voxels.points)
 
+    def save_coordinates(self, filename='data/coordinates.npy'):
+        # Save coordinates to the specified file
+        np.save(filename, self.coordinates)
+
+    def load_coordinates(self, filename='data/coordinates.npy'):
+        # Load coordinates from the specified file
+        self.coordinates = np.load(filename)
+
+    def plot_shell(self, plot_type="Surface"):
+        cloud = pv.PolyData(np.array(self.points_cloud))
+        surf = cloud.delaunay_3d()
+
+        if plot_type == "Surface":
+            surf.plot(show_edges=False)
+        else:
+            voxels = pv.voxelize(surf, check_surface=False)
+            voxels.plot()
+
+    def plot_slices(self):
+        sorted_points = self.coordinates[self.coordinates[:, 1].argsort()]
+        y_values = sorted_points[:, 1]
+        xz_pairs = sorted_points[:, [0, 2]]
+        transformed_list = [(y, xz_pairs[y_values == y]) for y in np.unique(y_values)]
+        transformed_array = np.array([(y, pairs) for y, pairs in transformed_list], dtype=object)
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+
+        for y, pairs in transformed_list[::5]:
+            x, z = pairs.T
+            ax.scatter(x, np.full_like(x, y), z, label=f"y = {y}")
+
+        ax.set_xlabel('X-axis')
+        ax.set_ylabel('Y-axis')
+        ax.set_zlabel('Z-axis')
+        ax.legend()
+        plt.show()
 
 if __name__ == "__main__":
 
-    # img_index = [i for i in range(90)] + [j for j in range(270,360)]
     img_index = [i for i in range(180)] 
     image_folder = "images/reconstruction"
     images = [f'{image_folder}/angle_{i}.jpg' for i in img_index]
