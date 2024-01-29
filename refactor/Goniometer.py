29450857f4adb56449133d4a3aeb1e51a973ec0f
Andre Meneses <andre_meneses2001@hotmail.com>
2024-01-29 15:41:37 -0300

    Improve modularity and class readability

--- refactor/Goniometer.py
+++ refactor/Goniometer.py
@@ -1,44 +1,46 @@
 import serial
-import numpy as np
 import curses
+import numpy as np
 from Camera import Camera
 from Image_processor import ImageProcessor
 from teste import find_contour
-import matplotlib.pyplot as plt
-from outils import show_wait_destroy
 import time
-import os
-import cv2 as cv
-from scipy.spatial import Delaunay
-from skimage import measure
-from skimage.measure import marching_cubes
 
 class GoniometerController:
+    STEPS_PER_DEGREE = 12800
+    MOVEMENT_INCREMENT = 0.15
+
     def __init__(self, port='/dev/ttyUSB0', baud_rate=115200):
         self.port = port
         self.baud_rate = baud_rate
-        self.steps_per_degree = 12800  # Assuming 12800 steps per degree
+        self.ser = None
+
+    def connect(self):
         self.ser = serial.Serial(self.port, self.baud_rate)
 
+    def disconnect(self):
+        if self.ser:
+            self.ser.close()
+
     def __enter__(self):
-        self.ser = serial.Serial(self.port, self.baud_rate)
+        self.connect()
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        self.ser.close()
-
+        self.disconnect()
+        
     def _parse_state(self, message):
         message = message.decode('utf-8').strip()
         return int(message[0])
 
-    def _prepare(self):
-        self.ser.write(b'SHH\r\n')       
-        self.ser.write(b'MTH = -2\r\n')  
-        self.ser.write(b'CN1\r\n')       
+    def _prepare_goniometer(self):
+        self.ser.write(b'SHH\r\n')
+        self.ser.write(b'MTH = -2\r\n')
+        self.ser.write(b'CN1\r\n')
 
     def move(self, angle, speed=60000, acc=5000, dec=5000, verbose=False):
-        self._prepare()
-        angle_steps = str(int(angle * self.steps_per_degree))
+        self._prepare_goniometer()
+        angle_steps = str(int(angle * self.STEPS_PER_DEGREE))
         commands = [
             f'PRH={angle_steps}\r\n',
             f'SPH={speed}\r\n',
@@ -62,92 +64,57 @@ class GoniometerController:
             if verbose:
                 print(state)
 
-
-    def move_manually(self,stdscr):
-        curses.noecho()
-        curses.cbreak()
-        stdscr.keypad(True)
-
-        while True:
-            stdscr.clear()
-            stdscr.refresh()
-
-            key = stdscr.getch()
-            movement = 0.15
-
-            if key == curses.KEY_LEFT:
-                self.move(-1)
-            elif key == curses.KEY_RIGHT:
-                self.move(1)
-            elif key == ord('q'):
-                break
-
-        curses.nocbreak()
-        stdscr.keypad(False)
-        curses.echo()
-        curses.endwin()
-
     def calibrate_goniometer(self, camera):
         angle = 0
-
         angle_areas = []
 
-        for i in range(50):
-            area = find_contour(i, camera)
-            angle_areas.append([angle,area])
-            self.move(0.1)
-            print(i)
-            angle += 0.1
+        try:
+            for i in range(50):
+                area = find_contour(i, camera)
+                angle_areas.append([angle, area])
+                self.move(self.MOVEMENT_INCREMENT)
+                print(i)
+                angle += self.MOVEMENT_INCREMENT
 
-        self.move(-5)
+            self.move(-5)
 
-        angle = 0
+            angle = 0
 
-        for i in range(50):
-            area = find_contour(i*-1, camera)
-            angle_areas.append([angle,area])
-            self.move(-0.1)
-            print(i*-1)
-            angle -= 0.1
+            for i in range(50):
+                area = find_contour(i * -1, camera)
+                angle_areas.append([angle, area])
+                self.move(-self.MOVEMENT_INCREMENT)
+                print(i * -1)
+                angle -= self.MOVEMENT_INCREMENT
 
-        angle_areas = np.array(angle_areas)
-        max_index = np.argmax(angle_areas[:,1])
-        mpoint = angle_areas[max_index]
+            angle_areas = np.array(angle_areas)
+            max_index = np.argmax(angle_areas[:, 1])
+            mpoint = angle_areas[max_index]
 
-        self.move(5)
+            self.move(5)
 
-        print(f"Max area:{mpoint[1]}, angle:{mpoint[0]}")
-        self.move(mpoint[0])
-        return mpoint[0]
+            print(f"Max area:{mpoint[1]}, angle:{mpoint[0]}")
+            self.move(mpoint[0])
+            return mpoint[0]
+        except Exception as e:
+            print(f"Error during calibration: {e}")
+            # Handle or log the exception as needed
 
-    def construct_3d_model(self):
+    def perform_tomography(self):
+
         camera = Camera(0)
 
-        for i in range(360):
-            camera.take_picture(f"images/reconstruction/angle_{i}.jpg")
-            self.move(1)
-        
-        # dataset_dir = "images/reconstruction"
-        # images = []
-
-        # for i in range(360):
-            # img_path = os.path.join(dataset_dir, f"angle_{i}.jpg")
-            # img = cv.imread(img_path, cv.IMREAD_GRAYSCALE)[65:250,130:500]
-            # images.append(img)
-            # return (img)
-            # show_wait_destroy('a',img)
+        try:
+            for i in range(360):
+                camera.take_picture(f"images/reconstruction/angle_{i}.jpg")
+                self.move(1)
+        except Exception as e:
+            print(f"Error during tomography: {e}")
+            # Handle or log the exception as needed
 
 if __name__ == '__main__':
     with GoniometerController() as controller:
-        # controller.ser.write(b'ST\r\n')
-        # controller.move(-90)
-        # angle1 = controller.calibrate_coordinates(0)
-        # time.sleep(30)
-        # controller.move(90)
-        # angle2 = controller.calibrate_coordinates(2)
-        # print(90 + angle2)
-         
-        # Ângulo entre camêras: 89° 
-        # controller.move(-89)
-        controller.construct_3d_model()
+        controller.connect()
+        controller.perform_tomography()
+        controller.disconnect()
 
